be98f02f4029bd5fe709d8a3605cc9178036e7e1
Rghyga <grvfyyad@katsuazaa.web.id>
2026-02-08 21:27:29 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // supaya api route pakai runtime nodejs (bukan edge)
-  experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium", "playwright-core"]
-  }
-};
-export default nextConfig;
